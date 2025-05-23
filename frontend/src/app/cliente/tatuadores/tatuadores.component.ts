e8195ed8fcb758bae0ae44f8b5f75fb8f79f941f
Miguel Angel Suarez Azuero <suarezazuerom@gmail.com>
2025-05-23 22:38:48 +0000

    Update docker-compose.yml

--- frontend/src/app/cliente/tatuadores/tatuadores.component.ts
+++ frontend/src/app/cliente/tatuadores/tatuadores.component.ts
@@ -3,6 +3,7 @@ import { RouterLink } from '@angular/router';
 import { PerfilEmpleado } from '../../modelos/perfil-empleado.model';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+//
 
 @Component({
   selector: 'app-tatuadores',
